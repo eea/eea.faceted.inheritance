4161c9715388093442589d521e910cecb0c6dcdf
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-04-19 10:39:57 +0000

    Tagged eea.faceted.inheritance 0.3.

---
+++ trunk/eea.faceted.inheritance/tags/0.3/eea/faceted/inheritance/browser/app/view.py
@@ -0,0 +1,5 @@
+from Products.Five.browser import BrowserView
+
+class ViewForm(BrowserView):
+    """ Faceted Heritor default view
+    """
---
+++ trunk/eea.faceted.inheritance/tags/0.3/eea/faceted/inheritance/interfaces.py
@@ -0,0 +1,7 @@
+# Subtypes
+from eea.faceted.inheritance.subtypes.interfaces import IPossibleFacetedHeritor #pylint: disable-msg = W0611
+from eea.faceted.inheritance.subtypes.interfaces import IFacetedHeritor #pylint: disable-msg = W0611
+
+# Criteria
+from eea.faceted.inheritance.criteria.interfaces import IHeritorAccessor #pylint: disable-msg = W0611
+from eea.faceted.inheritance.criteria.interfaces import ICriteria #pylint: disable-msg = W0611
