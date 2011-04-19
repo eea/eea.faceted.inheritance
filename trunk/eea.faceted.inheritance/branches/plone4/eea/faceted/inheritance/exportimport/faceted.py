11ca5f2bfdc3421227ff12c607df8c73599fcbc4
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-04-19 08:08:02 +0000

    Refs #8213   - Started plone4 migration.

---
+++ trunk/eea.faceted.inheritance/branches/plone4/eea/faceted/inheritance/exportimport/faceted.py
@@ -0,0 +1,18 @@
+from Products.GenericSetup.utils import XMLAdapterBase
+from eea.faceted.inheritance.interfaces import IFacetedHeritor
+
+class FacetedHeritorXMLAdapter(XMLAdapterBase):
+    __used_for__ = IFacetedHeritor
+
+    def _exportNode(self):
+        """Export the object as a DOM node.
+        """
+        node = self._getObjectNode('object')
+        return node
+
+    def _importNode(self, node):
+        """Import the object from the DOM node.
+        """
+        return
+
+    node = property(_exportNode, _importNode)
