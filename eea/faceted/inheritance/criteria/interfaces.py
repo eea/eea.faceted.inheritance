0031a7f8ac21b4f19068f29062511fa8c462aef7
tiberich <tiberiuichim@users.noreply.github.com>
2011-11-14 16:15:39 +0000

    Refs #8677; Move current trunk to plone25 branch

---
+++ eea/faceted/inheritance/criteria/interfaces.py
@@ -0,0 +1,6 @@
+from eea.facetednavigation.interfaces import ICriteria #pylint: disable-msg = W0611
+from zope.interface import Interface
+
+class IHeritorAccessor(Interface):
+    """ Heritor accessor/mutator
+    """
